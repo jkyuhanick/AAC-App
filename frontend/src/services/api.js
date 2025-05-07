7d2b67b3d9c546f12c66143b99d419ce7f0e685b
jkyuhanick <juliayuhanick@gmail.com>
2025-05-07 11:05:04 -0400

    update user auth

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -12,19 +12,6 @@ const api = axios.create({
   withCredentials: true,  
 });
 
-// interceptor to include the token in the Authorization header for each request
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('jwt');  // or sessionStorage.getItem('jwt')
-
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
-  }
-
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
-
 
 // Register a new user
 export const registerUser = async (email, password) => {
