656acfe229e0144435d5e35e1f0d2b69fe8e873b
jkyuhanick <juliayuhanick@gmail.com>
2025-07-23 10:19:35 -0400

    Update home

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Board from "../components/Board";
 import { getBoardById } from "../services/api";
-import { useNavigate } from "react-router-dom";
-import "../styles/styles.css"
-import "../styles/HomePage.css"
+import "../styles/styles.css";
+import "../styles/HomePage.css";
 
 const HomePage = ({ user, allBoards }) => {
   const [currentBoard, setCurrentBoard] = useState(null);
@@ -21,9 +21,8 @@ const HomePage = ({ user, allBoards }) => {
         }
       }
 
-      // Default to the first board if available
       if (allBoards.length > 0) {
-        setCurrentBoard(allBoards[0]);
+        setCurrentBoard(allBoards[0]); // Default board
       }
     };
 
@@ -34,17 +33,20 @@ const HomePage = ({ user, allBoards }) => {
     return <h3 className="user-alert">Please log in to view your boards.</h3>;
   }
 
-  return (
-    <div>
-      {currentBoard ? (
-        <Board board={currentBoard} />
-      ) : (
-        <div className="homePagediv">        
+  if (allBoards.length === 0) {
+    return (
+      <div className="homePagediv">
         <h3>No boards available. Create a new one!</h3>
         <button onClick={() => navigate("/create-board")} className="create-button">
-        <i className="fa-solid fa-plus"></i> &nbsp; Create New Board </button>
-        </div>
-      )}
+          <i className="fa-solid fa-plus"></i> &nbsp; Create New Board
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {currentBoard && <Board board={currentBoard} />}
     </div>
   );
 };
